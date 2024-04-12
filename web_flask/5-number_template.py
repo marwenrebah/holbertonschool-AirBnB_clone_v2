7072595c5eb71540083dd025ed445a5c2e0dc6cf
marwenrebah <6863@holbertonstudents.com>
2024-04-12 00:17:03 -1100

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -31,9 +31,9 @@ def number(n):
     return '{:d} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n> ', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', n = n)
 
 
 if __name__ == '__main__':
