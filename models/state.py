3518e4813dd97c97269ada8340b8d3fb32851817
marwenrebah <6863@holbertonstudents.com>
2024-05-18 03:45:57 -1100

    State Module

--- models/state.py
+++ models/state.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-import models
-from sqlalchemy import Column, String
+""" State Module for HBNB project """
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from models.city import City
+from models.base_model import Base
 from os import getenv
+from models.base_model import BaseModel
+from models.city import City
+import models
 
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database.
+    """State class"""
 
-    Inherits from SQLAlchemy Base and links to the MySQL table states.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        name (sqlalchemy String): The name of the State.
-        cities (sqlalchemy relationship): The State-City relationship.
-    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all, delete")
+        cities = relationship("City", backref="state", cascade="delete")
     else:
+
         @property
         def cities(self):
-            """Get a list of City instances with
-                state_id equals to the current State.id.
-
-            This is a getter attribute for FileStorage
-                relationship between State and City.
-            """
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """returns the list of City instances"""
+            instance_list = []
+            for ins in models.storage.all(City).values():
+                if ins.state_id == self.id:
+                    instance_list.append(ins)
+            return instance_list
