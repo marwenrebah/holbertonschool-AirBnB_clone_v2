dba67995d4c627d55d04324a41adc236089833dc
marwenrebah <6863@holbertonstudents.com>
2024-05-18 04:00:32 -1100

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"Start a Flask Web Application"
+"""
+Write a script that starts a Flask web application:
+"""
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+@app.route("/states_list", strict_slashes=False)
+def list_of_states():
+    """display a list of all State objects
+    present in DBStorage sorted by name"""
+
+    stateslist = {}
+    for state in storage.all(State).values():
+        stateslist[state.id] = state.name
+    stateslist = dict(sorted(stateslist.items(), key=lambda item: item[1]))
+    return render_template("7-states_list.html", statesdict=stateslist.items())
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes storage on teardown"""
+def remove_sess(self):
+    """removing the current SQLAlchemy Session after each request"""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
