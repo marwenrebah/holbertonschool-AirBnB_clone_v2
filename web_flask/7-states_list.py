313388557ce244f18735030dbfc5246e586c2a4c
marwenrebah <6863@holbertonstudents.com>
2024-07-22 23:48:49 -1100

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
+"""a script that start a web application"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
+    """Displays an HTML page with a list of all State objects in DBStorage"""
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """Remove the current session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
