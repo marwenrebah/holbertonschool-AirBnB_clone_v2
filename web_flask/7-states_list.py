cd60486f823b5376e9fd319d2ef20055a293f3d2
marwenrebah <6863@holbertonstudents.com>
2024-07-23 00:06:13 -1100

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""a script that start a web application"""
+"""
+flask model
+"""
 from flask import Flask, render_template
 from models import storage
-
-
+from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage"""
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current session"""
+def teardown_data(self):
+    """
+        refrech data
+    """
     storage.close()
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ return all states in the db  """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
